87c558af9785b523eee0a5e0845c61f48f5c0867
LuiCoro <luiacorona029@icloud.com>
2021-11-19 21:50:06 -0800

    'Updated Table' - tasks table created 'Updated Seed' - Dummy Data For Resources & Tasks were not letting me update my migrations, so I removed them for now

--- data/migrations/20211120042921_migrations.js
+++ data/migrations/20211120042921_migrations.js
@@ -1,4 +1,4 @@
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema
 
       .createTable('projects', tbl => {
@@ -16,8 +16,20 @@ exports.up = function(knex) {
         tbl.string('resource_description')
       })
 
+      .createTable('tasks', tbl => {
+        tbl.increments('task_id')
+
+        tbl.string('task_description').notNullable()
+        tbl.string('task_notes')
+        tbl.boolean('task_completed').defaultTo(false)
+        tbl.integer('project_id').unsigned().notNullable().references('project_id').inTable('projects')
+      })
+
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('projects')
+exports.down = function (knex) {
+  return knex.schema
+      .dropTableIfExists('projects')
+      .dropTableIfExists('resources')
+      .dropTableIfExists('tasks')
 };
