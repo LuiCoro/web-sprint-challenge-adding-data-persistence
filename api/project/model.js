fbe96211833e313a5ca6222ddbb6dd32fa3c6ca6
LuiCoro <luiacorona029@icloud.com>
2021-11-21 21:03:50 -0800

    'Router' - able to get all projects
    'Model'
    - Created getProjects
    
    [Testing]
    - 8 (TEST PASSED) out of 18 (TOTAL # OF TESTS)

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,21 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+const getProjects = async () => {
+  const rows = await db("projects");
+
+  const updatedRows = rows.map((row) => {
+    if (row.project_completed === 0) {
+      return { ...row, project_completed: false };
+    } else {
+      return { ...row, project_completed: true };
+    }
+  });
+
+  return updatedRows;
+};
+
+
+module.exports = {
+  getProjects
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,17 @@
 // build your `/api/projects` router here
+
+const router = require('express').Router()
+
+const Projects = require('./model')
+
+router.get('/', (req, res, next) => {
+  Projects.getProjects()
+      .then((projects) => {
+        res.status(200).json(projects)
+      })
+      .catch(next)
+})
+
+
+
+module.exports = router
