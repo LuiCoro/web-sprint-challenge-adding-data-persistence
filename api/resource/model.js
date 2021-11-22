f1bb47bddaf82abd1c52779c8307862a3819c94b
LuiCoro <luiacorona029@icloud.com>
2021-11-21 22:32:56 -0800

    'Tasks Router' - Able to get all Tasks - Able to get create new Tasks
    [Testing]
    - 17 (TEST PASSED) out of 18 (TOTAL # OF TESTS)

--- api/resource/model.js
+++ api/resource/model.js
@@ -5,8 +5,18 @@ const getResources = () => {
   return db('resources')
 }
 
+const getResourcesById = (id) => {
+  return db('resources').where('resource_id', id).first()
+}
 
+
+const createResource = async (resource) => {
+  const [id] = await  db('resources').insert(resource)
+
+  return getResourcesById(id)
+}
 
 module.exports = {
-  getResources
+  getResources,
+  createResource
 }
--- api/server.js
+++ api/server.js
@@ -5,10 +5,14 @@ const server = express()
 
 const projectsRouter = require('./project/router')
 const resourcesRouter = require('./resource/router')
+const taskRouter = require('./task/router')
 
 server.use(express.json())
 
 server.use('/api/projects', projectsRouter)
 server.use('/api/resources', resourcesRouter)
+server.use('/api/tasks', taskRouter)
+
+
 
 module.exports = server
